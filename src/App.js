1776f3f7e6b16a958dad4b24aa983caa02809b01
Kenneth <kenneth.aladi@xische.com>
2021-11-02 17:35:08 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import React, { Component } from 'react'
 import './App.css';
+import BarChart from "./components/BarChart";
+import Form from "./components/Form"
+import PieChart from './components/PieChart';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Skills Union Statistics App</p>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <BarChart />
+        <PieChart />
+        <Form />
+      </div>
+    )
+  }
 }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>Skills Union Statistics App</p>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
